31ff05ff5ceb56482571ad2327856cce48ad7bad
EliasVincent <riesyeti@outlook.de>
2023-04-17 20:53:22 +0200

    download from the server

--- renderer/pages/download-model.tsx
+++ renderer/pages/download-model.tsx
@@ -13,28 +13,37 @@ const windows = os.platform == "win32";
 function DownloadModel() {
   const progressBarRef = React.useRef(null);
   const [success, setSuccess] = React.useState(false);
+  const [downloadPercentValue, setDownloadPercentValue] = React.useState(0);
+  const [downloadStatusMessage, setDownloadStatusMessage] = React.useState("");
+  const [isDownloading, setIsDownloading] = React.useState(false);
 
   const downloadModel = () => {
-    const url = "https://gpt4all.io/ggml-gpt4all-j.bin"
-    const filename = "ggml-gpt4all-j.bin"
+    const url = "https://dl.eliasvsimon.com/model/gpt4all-lora-quantized.bin"
+    const filename = "gpt4all-lora-quantized.bin"
     const modelDir = rootModelDir
     const downloader = new Downloader({
       url: url,
       directory: modelDir,
       cloneFiles: true, // do not overwrite existing files
       onProgress: (progress) => {
-        console.log(progress);
-        progressBarRef.current.value = progress;
-      }
+        setIsDownloading(true);
+        setDownloadPercentValue(progress);
+        setDownloadStatusMessage("Downloading model...");
+        if (progress == 100.0) {
+          console.log("Download complete");
+          setDownloadStatusMessage("Download complete");
+          setSuccess(true);
+        }
+      },
+      onError: (err) => {
+        console.log(err);
+        setDownloadStatusMessage("Download failed" + err.toString());
+      },
     });
     downloader.download();
     downloader.on("end", () => {
-      console.log("Download complete");
+      console.log("Download complete, but this got reached???");
       progressBarRef.current.value = 100;
-    });
-    downloader.on("progress", (progress) => {
-      progressBarRef.current.value = progress;
-      console.log(progress);
     });
   }
 
@@ -50,7 +59,10 @@ function DownloadModel() {
             <h1 className='text-xl'>Download Model to default directory</h1>
             <button className="btn btn-primary" onClick={downloadModel}>Download Model</button>
             <div id="progress-bar">
-            <progress className="progress progress-primary w-56 border" value="0" max="100"></progress>
+              <progress className="progress progress-primary w-56 border" value={downloadPercentValue} max="100"></progress>
+            </div>
+            <div className="text-center text-xl">
+              {downloadStatusMessage == "" ? "" : downloadStatusMessage}
             </div>
           </div>
         </div>
